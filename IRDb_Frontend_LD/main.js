df6513e4f869e980b1ae55da567f972a0dcd7e33
Linneadb <linnea.damber@gmail.com>
2023-10-18 19:40:18 +0200

    Layout and color scheme

--- IRDb_Frontend_LD/main.js
+++ IRDb_Frontend_LD/main.js
@@ -7,7 +7,6 @@ fetch('https://localhost:7144/api/Movies')
 .then(res=> res.json())
 .then((data) => 
 {
-  //console.log(data)  
   MakeMovieCards(data)
 })
 .catch(error => console.log(error))
@@ -20,9 +19,7 @@ function MakeMovieCards(data)
           var newMovieCard = document.createElement('div');
           newMovieCard.className = "card movie-card";
           newMovieCard.id = `${movie.id}`;
-          //var urlTitle = "The little mermaid";
           var urlTitle = encodeURIComponent(movie.title)
-          console.log(urlTitle);
           var buttonUrl = `http://www.google.com/search?q=imdb+%22${urlTitle}%22&btnI=I%27m+Feeling+Lucky`
           const cardMarkup = `<div class="movie-card">
                               <h5 class="card-title">${movie.title}</h5>
@@ -41,89 +38,92 @@ function MakeMovieCards(data)
 
 //POST MOVIE
 
-//Selectors
-
-//const submitBtn = document.querySelector("#submit-btn");
-const form = document.querySelector('form');
-//const inputs = Array.from(document.querySelectorAll('input'));
-
+//Select submit-button
+const submitBtn = document.querySelector("#submit-btn");
 
 //Event Listener with add movie functionality
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+submitBtn.addEventListener('click', addMovie); 
 
-    // Read input content
-    const title = document.getElementById('title').value;
-    const director = document.getElementById('director').value;
-    const year = parseInt(document.getElementById('year').value);
-    const genre = document.getElementById('genre').value;
-    const duration = parseInt(document.getElementById('duration').value);
-    const rating = parseFloat(document.getElementById('rating').value);
+function addMovie()
+{
+  // Read input content
+  const title = document.getElementById('title').value;
+  const director = document.getElementById('director').value;
+  const year = parseInt(document.getElementById('year').value);
+  const genre = document.getElementById('genre').value;
+  const duration = parseInt(document.getElementById('duration').value);
+  const rating = parseFloat(document.getElementById('rating').value);
 
-    // Check for empty strings/Nan-values
-    if (!title || !director || isNaN(year) || isNaN(duration) || isNaN(rating)) {
-        alert('Please fill out all fields with valid values.');
-        return;
+  // Check for empty strings/Nan-values
+  if (!title || !director || isNaN(year) || isNaN(duration) || isNaN(rating)) {
+      const newMovie = {
+      title: title,
+      director: director,
+      year: year,
+      genre: genre,
+      duration: duration,
+      rating: rating
+  };
+  }
+
+  console.log(newMovie);
+
+  //POST newMovie to database
+
+  fetch('https://localhost:7144/api/Movies'
+  , {
+      method:"POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMovie),
+  });
+
+  //GET ALL MOVIES
+
+  fetch('https://localhost:7144/api/Movies')
+  .then(res=> res.json())
+  .then((data) => 
+  {
+    MakeMovieCards(data)
+  })
+  .catch(error => console.log(error))
+
+};
+
+
+
+//SEARCH MOVIE
+
+//Select search input
+const searchInput = document.getElementById('search').value;
+
+//Select search button
+const searchBtn = document.getElementById('search-btn')
+
+//Select all movie cards
+const movieCards = Array.from(document.querySelectorAll(".movie-card"))
+
+//Select all movie card titles
+const movieTitles = Array.from(document.querySelectorAll(".card-title"))
+
+const oneTitle = document.querySelector(".card-title")
+
+
+searchBtn.addEventListener('click', searchMovieTitle)
+
+function searchMovieTitle(){
+  console.log(searchInput)
+  console.log(searchBtn.innerText)
+  console.log(oneTitle)
+  console.log(movieTitles) //This is were I'm stuck...is the problem that I can not selct generated html?
+
+  movieTitles.forEach(title => {
+    console.log(title.innerText)
+    if(searchInput == title.innerText)
+    {      
+      title.parentElement.style.background = "#ffffff";
     }
-
-    // Create newMovie as JSON
-    const newMovie = {
-        title: title,
-        director: director,
-        year: year,
-        genre: genre,
-        duration: duration,
-        rating: rating
-    };
-
-    console.log(newMovie);
-
-    fetch('https://localhost:7144/api/Movies'
-    , {
-        method:"POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMovie),
-    });
-
-    //GET ALL MOVIES
-
-    fetch('https://localhost:7144/api/Movies')
-    .then(res=> res.json())
-    .then((data) => 
-    {
-      //console.log(data)  
-      MakeMovieCards(data)
-    })
-    .catch(error => console.log(error))
-});
-
-//
-
-//Functions
-
-//function addMovie() {
-  
-    
-
-    //TODO: Check for empty strings
-
-    //create movie to be sent to the api
-   
- 
-    //console.log(newMovie)
-    //send the movie to the api
-    //read input content, check for empty strings and create movie
-    //const newMovie = {};
-    //inputs.forEach(input => {
-      //if(input != null)
-        //newMovie[input.name] = input.value;
-   // });
-
-    //console.log(newMovie)
- // });
-
-
-
+  })
+}
 
